247e777c21911be435c07c9a80ee4e9ecba5bcc1
Florencia Nodar <fnodar@algeiba.com>
2019-01-18 23:17:19 -0300

    Se agrego componente de Header.js con su estilo css

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
-
+import Header from './componentes/header';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-       
+       <Header titulo='Clima React'/>
       </div>
     );
   }
