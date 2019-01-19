3d092d964ac8484759ea4a0ad113df2d35c74a1d
Florencia Nodar <fnodar@algeiba.com>
2019-01-18 23:34:45 -0300

    Se creo el componente de formulario.js en el cual se le agregó al app.js y se creó la maqueta

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import Header from './componentes/header';
+import Formulario from './componentes/formulario';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
        <Header titulo='Clima React'/>
+       <Formulario/>
       </div>
     );
   }
